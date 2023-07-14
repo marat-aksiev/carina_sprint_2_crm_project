6b1fcd99758bdfa89c84c571041d0deb2d423f96
marat-aksiev <aksievmarat@gmail.com>
2023-07-14 15:52:42 -0400

    first changes

--- src/test/java/com/crm/step_definitions/Hooks.java
+++ src/test/java/com/crm/step_definitions/Hooks.java
@@ -1,11 +1,11 @@
-package com.project_name.step_definitions;
+package com.crm.step_definitions;
 
 
 
 
-import com.project_name.utilities.BrowserUtils;
-import com.project_name.utilities.ConfigurationReader;
-import com.project_name.utilities.Driver;
+import com.crm.utilities.BrowserUtils;
+import com.crm.utilities.ConfigurationReader;
+import com.crm.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
--- src/test/java/com/crm/step_definitions/LoginStepDefs.java
+++ src/test/java/com/crm/step_definitions/LoginStepDefs.java
@@ -1,7 +1,7 @@
-package com.project_name.step_definitions;
+package com.crm.step_definitions;
 
-import com.project_name.pages.LoginPage;
-import com.project_name.utilities.ConfigurationReader;
+import com.crm.pages.LoginPage;
+import com.crm.utilities.ConfigurationReader;
 import io.cucumber.java.en.Given;
 public class LoginStepDefs {
 
