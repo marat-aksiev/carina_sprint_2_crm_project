5a5619700cb1a0ba7c39f014d76d0371931923b4
julialinkola <julia.belenko@gmail.com>
2023-07-19 22:36:46 -0400

    company structure pages added

--- src/test/java/com/crm/pages/CompanyStructurePage.java
+++ src/test/java/com/crm/pages/CompanyStructurePage.java
@@ -30,12 +30,25 @@ public class CompanyStructurePage {
     @FindBy(xpath="//div[@class='finder-box-tabs']/span[2]//span[2]")
     public WebElement companyButton;
 
+    // dropdown
     @FindBy(xpath = "//div[@class='company-structure']")
     public WebElement getDropdown;
 
     @FindBy(xpath="//div[@class='company-department-arrow']")
     public WebElement dropdownButton;
 
+    @FindBy(xpath="//input[@id='NAME']")
+    public WebElement departmentNameInputBox;
+
+    @FindBy(xpath="//select[@id='IBLOCK_SECTION_ID']")
+    public WebElement parentDepartmentDropdown;
+
+
+    @FindBy(xpath="//select[@id='IBLOCK_SECTION_ID']/option[6]")
+    public WebElement parentDepartmentDropdownOption;
+
+    @FindBy(xpath="//div[@class = 'popup-window-buttons']/span[.='Add']")
+    public WebElement addButton;
 
 
 
