4dfa3af08e5503aaa0c3a9349d9f071c8a5fe1c4
julialinkola <julia.belenko@gmail.com>
2023-07-19 22:37:09 -0400

    company structure step definitions added

---
+++ src/test/java/com/crm/step_definitions/CompanyStructure_StepDefinitions.java
@@ -0,0 +1,80 @@
+package com.crm.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CompanyStructure_StepDefinitions {
+    @Given("{string} is logged in as HR user")
+    public void isLoggedInAsHRUser(String arg0) {
+    }
+
+    @When("{string} navigates to the {string} menu")
+    public void navigatesToTheMenu(String arg0, String arg1) {
+    }
+
+    @And("{string} clicks on {string} button")
+    public void clicksOnButton(String arg0, String arg1) {
+    }
+
+    @And("{string} should see a pop-up window")
+    public void shouldSeeAPopUpWindow(String arg0) {
+    }
+
+    @When("{string} clicks on {string} text")
+    public void clicksOnText(String arg0, String arg1) {
+    }
+
+    @And("{string} clicks on {string} option")
+    public void clicksOnOption(String arg0, String arg1) {
+    }
+
+    @And("{string} adds details of new department")
+    public void addsDetailsOfNewDepartment(String arg0) {
+    }
+
+    @And("{string} clicks anywhere on pop-up window")
+    public void clicksAnywhereOnPopUpWindow(String arg0) {
+    }
+
+    @And("{string} should see {string} and {string} buttons")
+    public void shouldSeeAndButtons(String arg0, String arg1, String arg2) {
+    }
+
+    @And("{string} clicks {string} button")
+    public void clicksButton(String arg0, String arg1) {
+    }
+
+    @Then("{string} added new department successfully")
+    public void addedNewDepartmentSuccessfully(String arg0) {
+    }
+
+    @Given("{string} is logged in as Helpdesk user")
+    public void isLoggedInAsHelpdeskUser(String arg0) {
+    }
+
+    @And("{string} clicks on {string} menu button")
+    public void clicksOnMenuButton(String arg0, String arg1) {
+    }
+
+    @And("{string} is directed to the {string} page")
+    public void isDirectedToThePage(String arg0, String arg1) {
+    }
+
+    @Then("{string} does not have {string} option")
+    public void doesNotHaveOption(String arg0, String arg1) {
+    }
+
+    @Given("the user is logged in as a {string}>")
+    public void theUserIsLoggedInAsA(String arg0) {
+    }
+
+    @And("user navigates to the {string} menu")
+    public void userNavigatesToTheMenu(String arg0) {
+    }
+
+    @Then("user should see {string} page")
+    public void userShouldSeePage(String arg0) {
+    }
+}
