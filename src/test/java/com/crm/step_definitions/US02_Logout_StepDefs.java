632f246b81a24c4d772ab5544f2b0b6a70b883d1
Konsta_Cydeo <pashkov_kv@yahoo.com>
2023-07-21 12:36:38 -0400

    created step defs

---
+++ src/test/java/com/crm/step_definitions/US02_Logout_StepDefs.java
@@ -0,0 +1,24 @@
+package com.crm.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US02_Logout_StepDefs {
+    @When("user click on the profile name")
+    public void userClickOnTheProfileName() {
+    }
+
+    @And("user clicks Log Out button")
+    public void userClicksLogOutButton() {
+
+    }
+
+    @Then("user can see login page")
+    public void userCanSeeLoginPage() {
+    }
+
+    @Then("user can see {int} options under profile name")
+    public void userCanSeeOptionsUnderProfileName(int arg0) {
+    }
+}
