3d5386037d50db18bc0b4c32feb536b6f288a555
Konsta_Cydeo <pashkov_kv@yahoo.com>
2023-07-21 14:24:15 -0400

    created logoutPage and updated logout stepdef

--- src/test/java/com/crm/step_definitions/US02_Logout_StepDefs.java
+++ src/test/java/com/crm/step_definitions/US02_Logout_StepDefs.java
@@ -1,12 +1,17 @@
 package com.crm.step_definitions;
 
+import com.crm.pages.LogoutPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class US02_Logout_StepDefs {
+
+    LogoutPage logoutPage = new LogoutPage();
+
     @When("user click on the profile name")
     public void userClickOnTheProfileName() {
+        logoutPage.usernameButton.click();
     }
 
     @And("user clicks Log Out button")
@@ -18,7 +23,7 @@ public class US02_Logout_StepDefs {
     public void userCanSeeLoginPage() {
     }
 
-    @Then("user can see {int} options under profile name")
+    @Then("user can see five options under profile name")
     public void userCanSeeOptionsUnderProfileName(int arg0) {
     }
 }
