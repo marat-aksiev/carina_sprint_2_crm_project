f514ed162b137b3df5f11a5884883a7236997a6a
Maryna Cydeo <marynakamenna@gmail.com>
2023-07-21 14:59:36 -0400

    created SendMessage page

--- src/test/java/com/crm/pages/SendMessage.java
+++ src/test/java/com/crm/pages/SendMessage.java
@@ -1,9 +1,20 @@
 package com.crm.pages;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class SendMessage extends BasePage {
+public class SendMessage extends BasePage  {
 
-    //@FindBy()
-
+    @FindBy(xpath = "//span[text()='Message']")
+    public WebElement messageTab;
+    @FindBy(xpath = "//iframe[@class='bx-editor-iframe']")
+    public WebElement messageField;
+@FindBy(xpath = "//div[@id='feed-add-post-destination-container']")
+    public  WebElement employeeInputBox;
+@FindBy(xpath = "//a[@id='bx-destination-tag']")
+    public WebElement addEmployee;
+    @FindBy(xpath = "//a[@id='destination5446536_last_UA']")
+    public WebElement availableEmployee;
+    @FindBy(xpath = "//button[@id='blog-submit-button-save']")
+    public WebElement sendBtn;
 }
